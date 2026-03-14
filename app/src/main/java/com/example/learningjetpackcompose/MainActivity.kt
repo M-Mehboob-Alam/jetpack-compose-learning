5cbd52f6886da12c6f23dd590bf943af4159f669
Muzamil <calltoexperts@gmail.com>
2026-03-14 11:07:50 +0500

    Scaffold  Topbar bottom bar and floating point action button

--- app/src/main/java/com/example/learningjetpackcompose/MainActivity.kt
+++ app/src/main/java/com/example/learningjetpackcompose/MainActivity.kt
@@ -12,10 +12,14 @@ import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ElevatedButton
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FilledTonalButton
+import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -37,13 +41,42 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.learningjetpackcompose.ui.theme.LearningJetpackComposeTheme
 
+@OptIn(ExperimentalMaterial3Api::class)
 class MainActivity : ComponentActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             LearningJetpackComposeTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                Scaffold(
+
+                    topBar = {
+                        CenterAlignedTopAppBar(
+                            title = {
+                                Text(text = "Top App Bar")
+                            }
+                        )
+                    },
+                    bottomBar = {
+                        BottomAppBar(
+                            containerColor = Color.Blue,
+                            contentColor = Color.Gray,
+                            content = {
+                                Text(text = "Bottom App Bar")
+
+                            }
+                        )
+                    },
+                    floatingActionButton = {
+                        FloatingActionButton(onClick = {}) { }
+                    },
+
+                    modifier = Modifier.fillMaxSize(),
+
+
+                ) { innerPadding ->
 //                     var text by remember {
 //                        mutableStateOf("")
 //                    }
